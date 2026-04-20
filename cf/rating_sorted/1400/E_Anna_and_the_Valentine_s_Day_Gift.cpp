c9ba45a1b1bc8447f9100aede8c5c44fe0c64560
Arnav Mundada <arnav.mundada@vit.edu.in>
2026-04-20 11:13:52 +0530

    Add implementation for Codeforces problem "E. Anna and the Valentine's Day Gift"

---
+++ cf/rating_sorted/1400/E_Anna_and_the_Valentine_s_Day_Gift.cpp
@@ -0,0 +1,90 @@
+// ================== LIMITS REFERENCE ==================
+// INT_MAX        // int
+// INT_MIN
+//
+// LONG_MAX       // long
+// LONG_MIN
+//
+// LLONG_MAX      // long long
+// LLONG_MIN
+//
+// UINT_MAX       // unsigned int
+// ULONG_MAX      // unsigned long
+// ULLONG_MAX     // unsigned long long
+// ======================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// 1. Typedefs for faster typing
+using ll = long long;
+using vi = vector<int>;
+
+// ----- SIEVE TEMPLATE -----
+// TC: O(n log log n)
+// const int N = 1e7 + 10;
+// vector<bool> is_prime(N,1);
+//
+// void sieve(){
+//     is_prime[0] = is_prime[1] = 0;
+//     for(int i = 2; i < N; i++){
+//         if(is_prime[i])
+//             for(int j = 2 * i; j < N; j += i)
+//                 is_prime[j] = 0;
+//     }
+// }
+
+// 2. Fast I/O (crucial for competitive programming)
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void solve() {
+    int n,m;cin>>n>>m;
+    vector<ll> a(n);
+    for(auto &x:a) cin>>x;
+
+    vector<int> zero(n,0);
+    for(int i=0;i<n;i++)
+    {
+        ll x=a[i];
+        ll t=1e9;
+        for(int j=9;j>=1;j--)
+        {
+            if(x%t==0) {zero[i]+=j;break;}
+            t/=10;
+        }
+    }
+    sort(zero.rbegin(),zero.rend());
+
+    int digit=0;
+    for(ll x:a)
+    {
+        while(x>0)
+        {
+            digit++;
+            x/=10;
+        }
+    }
+    // cout<<digit;
+
+    for(int i=0;i<n;i+=2)
+    {
+        digit-=zero[i];
+    }
+
+    if(digit>m) cout<<"Sasha"<<endl;
+    else cout<<"Anna"<<endl;
+    
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
