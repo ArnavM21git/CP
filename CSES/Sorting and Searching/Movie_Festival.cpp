250c49273d972faea02fc99e6a1291d881a89a8c
Arnav Mundada <arnav.mundada@vit.edu.in>
2026-05-15 17:35:48 +0530

    Add implementations for CSES problems "Maximum Subarray Sum", "Movie Festival", and "Sum of Two Values"; include problem data and solution files

---
+++ CSES/Sorting and Searching/Movie_Festival.cpp
@@ -0,0 +1,78 @@
+// ================== LIMITS REFERENCE ==================
+// INT_MAX        // int
+// INT_MIN
+//
+// LONG_MAX       // long
+// LONG_MIN
+//
+// LLONG_MAX      // long long
+// LLONG_MIN
+//
+// UINT_MAX       // unsigned int
+// ULONG_MAX      // unsigned long
+// ULLONG_MAX     // unsigned long long
+// ======================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// 1. Typedefs for faster typing
+using ll = long long;
+using vi = vector<int>;
+
+// ----- SIEVE TEMPLATE -----
+// TC: O(n log log n)
+// const int N = 1e7 + 10;
+// vector<bool> is_prime(N,1);
+//
+// void sieve(){
+//     is_prime[0] = is_prime[1] = 0;
+//     for(int i = 2; i < N; i++){
+//         if(is_prime[i])
+//             for(int j = 2 * i; j < N; j += i)
+//                 is_prime[j] = 0;
+//     }
+// }
+
+// 2. Fast I/O (crucial for competitive programming)
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void solve() {
+    int n;cin>>n;
+    vector<pair<int,int>> v(n);
+    for(auto &p:v)
+    {
+        int x,y;cin>>x>>y;
+        p={x,y};
+    }
+    sort(v.begin(),v.end(),[](pair<int,int> a,pair<int,int> b){
+        if(a.second<b.second) return true;
+        else return false;
+    });
+
+    ll c=0;
+    int pendtime=0;
+    for(auto &x:v) 
+    {
+        if(x.first>=pendtime) {
+            c++;pendtime=x.second;
+        }
+    }
+    cout<<c;
+
+
+
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+  
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
