98fa0421ad3edf699b17fb451a3f2e72745559bc
Arnav Mundada <arnav.mundada@vit.edu.in>
2026-05-27 16:00:39 +0530

    Add implementations for CSES problems "Factory Machines" and "Room Allocation"; include problem data and solution files. Add implementations for Codeforces problems "A. Slimes on a Line", "B. Absolute Cinema", "C1. We Be Flipping (Easy Version)", and "C. Chipmunk Theo and Equality"; include problem data and solution files. Add template for competitive programming.

---
+++ cf/rating_sorted/1400/C_Chipmunk_Theo_and_Equality.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+using ll = long long;
+using vi = vector<int>;
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+template<typename K, typename V>
+using fast_o_map = gp_hash_table<K, V, custom_hash>;
+
+typedef tree<
+    int,
+    null_type,
+    less<int>,
+    rb_tree_tag,
+    tree_order_statistics_node_update
+> ordered_set;
+
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void solve() {
+    int n;cin>>n;
+    vector<int> a(n);
+    for(int &x:a) cin>>x;
+    
+    fast_o_map<int, int> cnt;
+    fast_o_map<int, ll> stepsum;
+    
+    for(int x:a)
+    {
+        set<int> s;
+        int currsteps=0;
+        while(!s.count(x))
+        {
+            cnt[x]++;
+            stepsum[x]+=currsteps;
+            s.insert(x);
+            if(x&1) x++,currsteps++;
+            else x/=2,currsteps++;
+        }
+    }
+    int mx=-1;
+    ll mncost=2e18;
+    for(auto &[x,co]:cnt)
+    {
+        if(co==n&&stepsum[x]<mncost) {
+            mx=x;mncost=stepsum[x];
+        }
+    }
+    ll steps=0;
+    for(int x:a)
+    {
+        while(x!=mx)
+        {
+            if(x&1) x++,steps++;
+            else x/=2,steps++;
+        }
+    }
+     cout<<steps<<"\n";
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ cf/rating_sorted/1400/template.cpp
@@ -0,0 +1,90 @@
+// ================== LIMITS REFERENCE ==================
+// INT_MAX        // int
+// INT_MIN
+//
+// LONG_MAX       // long
+// LONG_MIN
+//
+// LLONG_MAX      // long long
+// LLONG_MIN
+//
+// UINT_MAX       // unsigned int
+// ULONG_MAX      // unsigned long
+// ULLONG_MAX     // unsigned long long
+// ======================================================
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+// 1. Typedefs for faster typing
+using ll = long long;
+using vi = vector<int>;
+
+// ----- CODEFORCES ANTI-HASH SHIELD -----
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+gp_hash_table 3rd arg is custom_hash to prevent CF collision attacks
+
+
+// ----- PBDS ORDERED SET -----
+typedef tree<
+    int,
+    null_type,
+    less<int>,
+    rb_tree_tag,
+    tree_order_statistics_node_update
+> ordered_set;
+
+// Functions:
+// find_by_order()
+// order_of_key()
+
+// ----- SIEVE TEMPLATE -----
+// TC: O(n log log n)
+// const int N = 1e7 + 10;
+// vector<bool> is_prime(N,1);
+//
+// void sieve(){
+//     is_prime[0] = is_prime[1] = 0;
+//     for(int i = 2; i < N; i++){
+//         if(is_prime[i])
+//             for(int j = 2 * i; j < N; j += i)
+//                 is_prime[j] = 0;
+//     }
+// }
+
+// 2. Fast I/O (crucial for competitive programming)
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void solve() {
+    // 🚀 Example usage of your new weapon:
+    // fast_map<int, int> cnt;
+    // fast_map<int, ll> stepsum;
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
