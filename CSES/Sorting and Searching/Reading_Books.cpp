88713f51a4e294e7e0d7c6e0f58aa98d4e1afc9b
Arnav Mundada <arnav.mundada@vit.edu.in>
2026-05-28 17:06:01 +0530

    Add implementations for CSES problems "Reading Books" and "Tasks and Deadlines"; include problem data and solution files. Add template for Codeforces problem "o".

---
+++ CSES/Sorting and Searching/Reading_Books.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+using ll = long long;
+using vi = vector<int>;
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+template<typename K, typename V>
+using fast_o_map = gp_hash_table<K, V, custom_hash>;
+
+typedef tree<
+    int,
+    null_type,
+    less<int>,
+    rb_tree_tag,
+    tree_order_statistics_node_update
+> ordered_set;
+
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void solve() {
+    int n;cin>>n;
+    vector<ll> a(n);
+    for(auto &x:a) cin>>x;
+    ll sum=accumulate(a.begin(),a.end(),0LL);
+    cout<<max(sum,2*(*max_element(a.begin(),a.end())));
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
