a971b9b238e760bd1a7af8466057b8c34a60c41c
Arnav Mundada <arnav.mundada@vit.edu.in>
2026-05-31 08:20:41 +0530

    Add solutions for various Codeforces problems
    - Implement B. Digit String (Rating 1000)
    - Implement B. Remilia Plays Soku (Rating 1100)
    - Add problem statement for A. Convergence (Rating 1400)
    - Add problem statement for B. Cake Leveling (Rating 1400)
    - Update problem statement for B. Remilia Plays Soku (Rating 1400)
    - Add problem statement for C1. Seating Arrangement (Easy Version) (Rating 1400)
    - Implement A. Convergence (Rating 1400)
    - Implement B. Cake Leveling (Rating 1400)
    - Implement C1. Seating Arrangement (Easy Version) (Rating 1400)
    - Add problem statement for C1. Cirno and Number (Easy Version) (Rating 1500)
    - Implement C1. Cirno and Number (Easy Version) (Rating 1500)
    - Implement A. Construct an Array (Rating 800)
    - Implement A. Marisa Steals Reimu's Takeout (Rating 800)
    - Implement A. Optimal Purchase (Rating 800)
    - Implement A. Slimes on a Line (Rating 800)
    - Implement B. Absolute Cinema (Rating 800)
    - Implement B. Another Sorting Problem (Rating 800)

---
+++ CSES/Sorting and Searching/Subarray_Sums_I.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+using ll = long long;
+using vi = vector<int>;
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+template<typename K, typename V>
+using fast_o_map = gp_hash_table<K, V, custom_hash>;
+
+typedef tree<
+    int,
+    null_type,
+    less<int>,
+    rb_tree_tag,
+    tree_order_statistics_node_update
+> ordered_set;
+
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void solve() {
+    int n,x;cin>>n>>x;
+    vector<int> a(n);
+    for(auto &x:a) cin>>x;
+    vector<ll> prefix(n+1,0);
+    for(int i=1;i<=n;i++) prefix[i]=a[i-1]+prefix[i-1];
+    int l=1;ll c=0;
+    for(int r=1;r<=n;r++)
+    {
+        while(prefix[r]-prefix[l-1]>x) l++;
+        if(prefix[r]-prefix[l-1]==x) c++;
+    }
+    cout<<c;
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
--- cf/rating_sorted/1100/B_Remilia_Plays_Soku.cpp
+++ cf/rating_sorted/1100/B_Remilia_Plays_Soku.cpp
@@ -43,7 +43,7 @@ void fast_io() {
 void solve() {
     int n,x1,x2,k;cin>>n>>x1>>x2>>k;
     int val=min(abs(x1-x2),n-abs(x1-x2));
-    if(n<4) {cout<<val<<endl;return;}
+    if(n<4) {cout<<1<<endl;return;}
     cout<<val+k<<endl;
 }
 
