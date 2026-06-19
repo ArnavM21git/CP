dfee2e0c7bb57ee3a6c4344a82bd4961a85e6873
Arnav Mundada <arnav.mundada@vit.edu.in>
2026-06-19 21:09:43 +0530

    Add problem files and implementations for Codeforces problems "B. Artistic Balance Tree", "B. Zhily and Mex and Max", "A. Zhily and Bracket Swapping", and "A. Grid L"; include test cases and output.

---
+++ Codeforces/rating_sorted/1200/B_Zhily_and_Mex_and_Max.cpp
@@ -0,0 +1,136 @@
+// ================== LIMITS REFERENCE ==================
+// INT_MAX        // int
+// INT_MIN
+//
+// LONG_MAX       // long
+// LONG_MIN
+//
+// LLONG_MAX      // long long
+// LLONG_MIN
+//
+// UINT_MAX       // unsigned int
+// ULONG_MAX      // unsigned long
+// ULLONG_MAX     // unsigned long long
+// ======================================================
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+// ----- TYPEDEFS -----
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<long long>;
+
+// ----- MACROS -----
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define per(i, a, b) for (int i = (a); i >= (b); i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define ff first
+#define ss second
+
+// ----- CUSTOM HASH -----
+struct custom_hash
+{
+    static uint64_t splitmix64(uint64_t x)
+    {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const
+    {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+// Fast Hash Map
+template <typename K, typename V>
+using fast_o_map = gp_hash_table<K, V, custom_hash>;
+
+// ----- PBDS -----
+typedef tree<
+    int,
+    null_type,
+    less<int>,
+    rb_tree_tag,
+    tree_order_statistics_node_update>
+    ordered_set;
+
+// find_by_order(k)
+// order_of_key(x)
+
+// ----- SIEVE TEMPLATE -----
+// TC: O(n log log n)
+// const int N = 1e7 + 10;
+// vector<bool> is_prime(N, true);
+//
+// void sieve() {
+//     is_prime[0] = is_prime[1] = false;
+//
+//     for (int i = 2; i * i < N; i++) {
+//         if (is_prime[i]) {
+//             for (int j = i * i; j < N; j += i) {
+//                 is_prime[j] = false;
+//             }
+//         }
+//     }
+// }
+
+// Fast I/O
+void fast_io()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &x : a)
+        cin >> x;
+    sort(a.rbegin(), a.rend());
+    ll ans = 1LL * n * a[0];
+    map<int, int> freq;
+    for (int i = 0; i < n; i++)
+    {
+        freq[a[i]]++;
+    }
+    int mex = 0;
+    while (n--)
+    {
+        if (mex == a[0])
+            freq[mex++]--;
+        ans += mex;
+        if (freq[mex] > 0)
+            freq[mex++]--;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast_io();
+
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
