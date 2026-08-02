b569cc0ed499aded8d1a3d9568378e04c63570bc
Arnav Mundada <arnav.mundada@vit.edu.in>
2026-08-02 09:29:26 +0530

    Refactor "B. Mashmokh and ACM" code for improved readability; update author information, type definitions, and add utility functions.

--- Codeforces/rating_sorted/1400/B_Mashmokh_and_ACM.cpp
+++ Codeforces/rating_sorted/1400/B_Mashmokh_and_ACM.cpp
@@ -1,3 +1,6 @@
+/*
+author:  Arnav M
+*/
 // ================== LIMITS REFERENCE ==================
 // INT_MAX        // int
 // INT_MIN
@@ -20,20 +23,36 @@
 using namespace std;
 using namespace __gnu_pbds;
 
-// 1. Typedefs for faster typing
-using ll = long long;
-using vi = vector<int>;
+// ----- TYPEDEFS -----
+using ll = int64_t;
+using vi = vector<ll>;
+using vll = vector<ll>;
+
+// ----- MACROS -----
+#define int int64_t
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define per(i, a, b) for (int i = (a); i >= (b); i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define ff first
+#define ss second
+#define adder(v) accumulate(all(v), 0LL)
 
 // ----- CUSTOM HASH -----
-struct custom_hash {
-    static uint64_t splitmix64(uint64_t x) {
+struct custom_hash
+{
+    static uint64_t splitmix64(uint64_t x)
+    {
         x += 0x9e3779b97f4a7c15;
         x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
         x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
         return x ^ (x >> 31);
     }
 
-    size_t operator()(uint64_t x) const {
+    size_t operator()(uint64_t x) const
+    {
         static const uint64_t FIXED_RANDOM =
             chrono::steady_clock::now().time_since_epoch().count();
 
@@ -42,7 +61,7 @@ struct custom_hash {
 };
 
 // Fast Hash Map
-template<typename K, typename V>
+template <typename K, typename V>
 using fast_o_map = gp_hash_table<K, V, custom_hash>;
 
 // ----- PBDS -----
@@ -51,48 +70,156 @@ typedef tree<
     null_type,
     less<int>,
     rb_tree_tag,
-    tree_order_statistics_node_update
-> ordered_set;
+    tree_order_statistics_node_update>
+    ordered_set;
 
 // find_by_order(k)
 // order_of_key(x)
 
-// ----- SIEVE TEMPLATE -----
+// ================== SIEVE OF ERATOSTHENES ==================
+
+// ----- BASIC SIEVE -----
 // TC: O(n log log n)
 // const int N = 1e7 + 10;
-// vector<bool> is_prime(N, 1);
+// vector<bool> is_prime(N, true);
+
+// void sieve_basic() {
+//     is_prime[0] = is_prime[1] = false;
 //
-// void sieve() {
-//     is_prime[0] = is_prime[1] = 0;
-//     for (int i = 2; i < N; i++) {
+//     for (int i = 2; i <= N - 1; i++) {
 //         if (is_prime[i]) {
-//             for (int j = 2 * i; j < N; j += i) {
-//                 is_prime[j] = 0;
+//             for (int j = 2 * i; j <= N - 1; j += i) {
+//                 is_prime[j] = false;
 //             }
 //         }
 //     }
 // }
 
-// 2. Fast I/O
-void fast_io() {
+// ----- OPTIMIZED SIEVE -----
+// TC: O(n log log n)
+// const int N = 1e7 + 10;
+// vector<bool> is_prime(N, true);
+//
+// void sieve() {
+//     is_prime[0] = is_prime[1] = false;
+//
+//     for (int i = 2; i * i < N; i++) { // or i*i <= N-1
+//         if (is_prime[i]) {
+//             for (int j = i * i; j < N; j += i) { // or j <= N-1
+//                 is_prime[j] = false;
+//             }
+//         }
+//     }
+// }
+
+// Fast I/O
+void fast_io()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 }
 
-const int mod=1e9+7;
-void solve() {
-    int n,k;cin>>n>>k;
-    ll ans=((n-1)*(k))%mod;
-    12 20 15 20
+int binexp(int a, int b, int mod) // if mod is 10^18 then binmul
+{
+    a %= mod;
+    int res = 1;
+    while (b)
+    {
+        if (b & 1)
+        {
+            res = (res * a) % mod; // if mod~10^18: res=binmul(res,a,mod);
+        }
+        b >>= 1;
+        a = (a * a) % mod; // if mod~10^18: a=binmul(a,a,mod);
+    }
+    return res;
 }
 
-int main() {
+int binmul(int a, int b, int mod) // if mod is 10^18 then binmul
+{
+    a %= mod;
+    int res = 0;
+    while (b)
+    {
+        if (b & 1)
+        {
+            res = (res + a) % mod;
+        }
+        b >>= 1;
+        a = (a + a) % mod;
+    }
+    return res;
+}
+int mex(vi &a)
+{
+    int n = a.size();
+    vector<int> freq(n + 1, 0);
+    for (int x : a)
+    {
+        freq[x]++;
+    }
+    rep(i, 0, a.size())
+    {
+        if (freq[i] == 0)
+            return i;
+    }
+    return n;
+}
+// ----- INTERACTIVE QUERY -----
+int ask(int a, int b)
+{
+    cout << "? " << a << " " << b << endl;
+    int res;
+    cin >> res;
+    return res;
+}
+const int N = 2000;
+vector<vi> divisor(N + 1,vi (1,1));
+void precomp()
+{
+    
+    for (int i = 2; i <= N; i++)
+    {
+        for (int j =  i; j <= N ; j += i)
+        {
+            divisor[j].push_back(i);
+        }
+    }
+}
+const int m=1e9+7;
+void solve()
+{
+    int n,k;cin>>n>>k;
+    vector<vi> dp(k+1,vi(n+1,0));
+    rep(i,1,k+1) {dp[i][1]=1;}
+    rep(i,1,n+1) {dp[1][i]=1;}
+    rep(i,2,k+1)
+    {
+        rep(j,2,n+1)
+        {
+            for(auto &x:divisor[j])
+            {
+                dp[i][j]=(dp[i][j]+dp[i-1][x])%m;
+            }
+        }
+    }
+    ll ans=0;
+    for(int i=1;i<=n;i++)
+    {
+        ans=(ans+dp[k][i])%m;
+    }
+    cout<<ans;
+}
+
+int32_t main()
+{
     fast_io();
-
+    precomp();
     int t = 1;
-    // cin >> t;
+   
 
-    while (t--) {
+    while (t--)
+    {
         solve();
     }
 
