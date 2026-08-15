a41e14440f8dd95b09625fc5e3a44d7c6a73d9c2
Arnav Mundada <arnav.mundada@vit.edu.in>
2026-08-15 16:02:41 +0530

    Add solutions for Codeforces problems "A. Hot Potatoes at the Fairy Warehouse" and "B. Always Changing"; include problem details and test cases.

---
+++ Codeforces/rating_sorted/1200/A_Hot_Potatoes_at_the_Fairy_Warehouse.cpp
@@ -0,0 +1,205 @@
+/*
+author:  Arnav M
+*/
+// ================== LIMITS REFERENCE ==================
+// INT_MAX        // int
+// INT_MIN
+//
+// LONG_MAX       // long
+// LONG_MIN
+//
+// LLONG_MAX      // long long
+// LLONG_MIN
+//
+// UINT_MAX       // unsigned int
+// ULONG_MAX      // unsigned long
+// ULLONG_MAX     // unsigned long long
+// ======================================================
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+// ----- TYPEDEFS -----
+using ll = int64_t;
+using vi = vector<ll>;
+using vll = vector<ll>;
+
+// ----- MACROS -----
+#define int int64_t
+#define rep(i,a,b) for(int i=(a); i<(b); i++)
+#define per(i,a,b) for(int i=(a); i>=(b); i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define ff first
+#define ss second
+#define adder(v) accumulate(all(v), 0LL)
+
+// ----- CUSTOM HASH -----
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+// Fast Hash Map
+template<typename K, typename V>
+using fast_o_map = gp_hash_table<K, V, custom_hash>;
+
+// ----- PBDS -----
+typedef tree<
+    int,
+    null_type,
+    less<int>,
+    rb_tree_tag,
+    tree_order_statistics_node_update
+> ordered_set;
+
+// find_by_order(k)
+// order_of_key(x)
+
+// ================== SIEVE OF ERATOSTHENES ==================
+
+// ----- BASIC SIEVE -----
+// TC: O(n log log n)
+// const int N = 1e7 + 10;
+// vector<bool> is_prime(N, true);
+
+// void sieve_basic() {
+//     is_prime[0] = is_prime[1] = false;
+//
+//     for (int i = 2; i <= N - 1; i++) {
+//         if (is_prime[i]) {
+//             for (int j = 2 * i; j <= N - 1; j += i) {
+//                 is_prime[j] = false;
+//             }
+//         }
+//     }
+// }
+
+
+// ----- OPTIMIZED SIEVE -----
+// TC: O(n log log n)
+// const int N = 1e7 + 10;
+// vector<bool> is_prime(N, true);
+//
+// void sieve() {
+//     is_prime[0] = is_prime[1] = false;
+//
+//     for (int i = 2; i * i < N; i++) { // or i*i <= N-1
+//         if (is_prime[i]) {
+//             for (int j = i * i; j < N; j += i) { // or j <= N-1
+//                 is_prime[j] = false;
+//             }
+//         }
+//     }
+// }
+
+// Fast I/O
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+int binexp(int a, int b, int mod) //if mod is 10^18 then binmul
+{
+    a %= mod;
+    int res = 1;
+    while(b)
+    {
+        if(b & 1)
+        {
+            res = (res * a) % mod;//if mod~10^18: res=binmul(res,a,mod);
+        }
+        b >>= 1;
+        a = (a * a) % mod; //if mod~10^18: a=binmul(a,a,mod);
+    }
+    return res;
+}
+
+int binmul(int a, int b, int mod) //if mod is 10^18 then binmul
+{
+    a %= mod;
+    int res = 0;
+    while(b)
+    {
+        if(b & 1)
+        {
+            res = (res + a) % mod;
+        }
+        b >>= 1;
+        a = (a + a) % mod;
+    }
+    return res;
+}
+int mex(vi &a)
+{
+   int n=a.size();
+   vector<int> freq(n+1,0);
+   for(int x:a)
+   {
+     freq[x]++;
+   }
+   rep(i,0,a.size())
+   {
+     if(freq[i]==0) return i;
+   }
+   return n;
+}
+// ----- INTERACTIVE QUERY -----
+int ask(int a, int b) {
+    cout << "? " << a << " " << b << endl;
+    int res;
+    cin >> res;
+    return res;
+}
+
+void solve() {
+    int n,k;cin>>n>>k;string s;cin>>s;
+    int r=0,b=0;
+    rep(i,0,2*n)
+    {
+        if(s[i]=='1')
+        {
+        if(s[(i+1)%(2*n)]=='0')
+        {
+            if(i&1) b++;
+            else r++;
+        }
+        else{
+            if(i&1) r++;
+            else b++;
+        }
+        }
+    }
+    cout<<r<<" "<<b<<endl;
+
+}
+
+int32_t main() {
+    fast_io();
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
